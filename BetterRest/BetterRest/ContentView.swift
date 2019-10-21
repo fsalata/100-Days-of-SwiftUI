1e63418e63c18e21663f5c47fcd24863ca01be33
Fábio Salata <s2it_fsalata@uolinc.com>
2019-10-21 15:27:13 -0300

    BetterRest project added

---
+++ BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Fábio Salata on 21/10/19.
+//  Copyright © 2019 Fábio Salata. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
